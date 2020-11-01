23fc08161cd732d3e5a4594aa8c5587bf7adb01a
Glenn Nagel <glenn@mercury-wireless.com>
2020-11-01 14:39:10 -0500

    Updated CLI args

--- cmd/root.go
+++ cmd/root.go
@@ -14,13 +14,6 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "nginx-config-formatter",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
